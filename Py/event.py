a98c096e39e7ccba8b2f02c6aea367077d6c43d1
ghulamyaseen778 <muhammadyaseeen3294@gmail.com>
2023-11-30 23:23:21 +0500

    f

--- Py/event.py
+++ Py/event.py
@@ -40,19 +40,37 @@
 # if __name__ == "__main__":
 #     asyncio.run(main())
 
-import sys
-from cefpython3 import cefpython as cef
+# import sys
+# from cefpython3 import cefpython as cef
 import pathlib
 
-file_path = "C:\Remote-Work\plancck\dist\index.html"
-file_uri = pathlib.Path(file_path).as_uri()
+# file_path = "C:\Remote-Work\plancck\dist\index.html"
+# file_uri = pathlib.Path(file_path).as_uri()
 
-def main():
-    sys.excepthook = cef.ExceptHook
-    cef.Initialize()
-    browser = cef.CreateBrowserSync(url=file_uri,window_title='CEF Python React Example')
-    cef.MessageLoop()
-    cef.Shutdown()
+# def main():
+#     sys.excepthook = cef.ExceptHook
+#     cef.Initialize()
+#     browser = cef.CreateBrowserSync(url=file_uri,window_title='CEF Python React Example')
+#     cef.MessageLoop()
+#     cef.Shutdown()
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
+
+
+import sys
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+app = QApplication(sys.argv)
+web_view = QWebEngineView()
+# Load the React app
+web_view.setUrl(QUrl.fromLocalFile(r"C:\Remote-Work\plancck\index.html"))
+
+# Set window properties
+web_view.setWindowTitle('Planck')
+web_view.resize(800, 600)
+web_view.show()
+
+sys.exit(app.exec_())
